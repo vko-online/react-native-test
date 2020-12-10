1fdd422faab9ba6c4455bc6cbfc80e08049e7f85
Edel Sanchez <edel@outliant.com>
2020-12-11 06:41:51 +0800

    config eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,27 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+  env: {
+    browser: true,
+    es6: true,
+  },
+  parser: 'babel-eslint',
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+  },
 };
